64bd3978e142468bb8b66830f56a8d8f51a96b5d
Joacim Boive <joacim.boive@gmail.com>
2017-04-03 16:49:57 +0200

    It's a start....

--- src/statistics/js/script.js
+++ src/statistics/js/script.js
@@ -32,125 +32,32 @@
             })
     };
 
-    let getIndex = async (storage) => {
-        let lookups = [];
+    let getJIRAs = (url, jql) => {
+        let logDates = ` AND updatedDate >= ${document.getElementById('dateFrom').value} AND updatedDate <= ${document.getElementById('dateTo').value}`;
 
-        headers = new Headers({
-            'Authorization': 'Basic ' + toBase64(storage.login + ':' + storage.password),
-            'Content-Type': 'application/json'
-        });
-
-        const epic = await getEpicInfo(storage.url);
-
-        return new Promise((resolve, reject) => {
-                storage.fromDate = document.getElementById('dateFrom').value;
-                storage.toDate = document.getElementById('dateTo').value;
-
-                let logDates = ` AND updatedDate >= ${storage.fromDate} AND updatedDate <= ${storage.toDate}`;
-
-                fetch(`${storage.url}/rest/api/2/field`, {
-                    method: 'GET',
-                    redirect: 'follow',
-                    headers: headers
-                })
-                    .then(response => {
-                        return response.json()
-                    })
-                    .then(fields => {
-                        let link = fields.find((field) => {
-                            return field.name === 'Epic Link';
-                        });
-
-                        let name = fields.find((field) => {
-                            return field.name === 'Epic Name';
-                        });
-
-                        storage.epic = {};
-                        storage.epic.name = name.id;
-                        storage.epic.link = link.id;
-
-                    }).then(
-                    fetch(`${storage.url}/rest/api/2/search?jql=${storage.jql + logDates}&maxResults=1000`, {
-                        method: 'GET',
-                        redirect: 'follow',
-                        headers: headers
-                    })
-                        .then(response => {
-                            return response.json()
-                        })
-                        .then(jiras => {
-                                storage.fromDate = new Date(storage.fromDate);
-                                storage.toDate = new Date(storage.toDate);
-                                storage.report = {};
-                                storage.report.assignee = {};
-                                storage.report.total = 0;
-                                storage.report.issues = {};
-
-                                for (let issue of jiras.issues) {
-                                    let key = issue.fields.assignee.key;
-                                    let data = {};
-
-                                    storage.report.total++;
-
-                                    if (!storage.report.assignee[key]) {
-                                        storage.report.assignee[key] = {};
-                                        storage.report.assignee[key].aggregatetimeoriginalestimate = 0;
-                                        storage.report.assignee[key].aggregatetimespent = 0;
-                                        storage.report.assignee[key].index = 0;
-                                        storage.report.assignee[key].total = 0;
-                                        storage.report.assignee[key].displayName = '';
-                                        storage.report.assignee[key].data = [];
-                                        storage.report.assignee[key].epic = '';
-                                    }
-
-                                    storage.report.assignee[key].displayName = issue.fields.assignee.displayName;
-                                    storage.report.assignee[key].aggregatetimeoriginalestimate += issue.fields.aggregatetimeoriginalestimate;
-                                    storage.report.assignee[key].aggregatetimespent += issue.fields.aggregatetimespent;
-                                    storage.report.assignee[key].isSubtask = issue.fields.issuetype.subtask;
-                                    storage.report.assignee[key].total++;
-
-                                    data.aggregatetimeoriginalestimate = issue.fields.aggregatetimeoriginalestimate;
-                                    data.aggregatetimespent = issue.fields.aggregatetimespent;
-
-                                    storage.report.assignee[key].data.push(data);
-
-                                    if (storage.report.assignee[key].isSubtask) {
-                                        lookups.push(
-                                            fetch(issue.fields.parent.self, {
-                                                method: 'GET',
-                                                redirect: 'follow',
-                                                headers: headers,
-                                                data: storage.report.assignee[key]
-                                            })
-                                                .then(response => {
-                                                    return response.json()
-                                                })
-                                                .then(parentIssue => {
-
-                                                    lookups.push(setEpicName(data, parentIssue, storage.epic, storage.url));
-                                                })
-                                        )
-                                    } else {
-                                        lookups.push(setEpicName(storage.report.assignee[key], issue, storage.epic, storage.url));
-                                    }
-                                }
-
-                                Promise.all(lookups).then(() => {
-                                    console.table(storage.report);
-                                    resolve(storage.report);
-                                }, reason => {
-                                    debugger;
-                                    console.log(reason);
-                                    reject(reason);
-                                });
-                            }
-                        )
-                )
-            }
-        )
+        return fetch(`${url}/rest/api/2/search?jql=${jql + logDates}&maxResults=1000`, {
+            method: 'GET',
+            redirect: 'follow',
+            headers: headers
+        })
+            .then(response => {
+                return response.json()
+            })
     };
 
-    let setEpicName = (data, issue, epic, url) => {
+    let getParent = (issue) => {
+        return fetch(issue.fields.parent.self, {
+            method: 'GET',
+            redirect: 'follow',
+            headers: headers
+        })
+            .then(response => {
+                return response.json()
+            })
+
+    };
+
+    let getEpicName = (issue, epic, url) => {
         return fetch(`${url}/rest/api/2/issue/${issue.fields[epic.link]}`, {
             method: 'GET',
             redirect: 'follow',
@@ -160,8 +67,72 @@
                 return response.json()
             })
             .then(thisEpic => {
-                data.epic = thisEpic.fields[epic.name];
+                return thisEpic.fields[epic.name];
             })
+    };
+
+    let getIndex = async (storage) => {
+        headers = new Headers({
+            'Authorization': 'Basic ' + toBase64(storage.login + ':' + storage.password),
+            'Content-Type': 'application/json'
+        });
+
+        const epic = await getEpicInfo(storage.url);
+        const jiras = await getJIRAs(storage.url, storage.jql);
+
+        storage.fromDate = new Date(storage.fromDate);
+        storage.toDate = new Date(storage.toDate);
+        storage.report = {};
+        storage.report.assignee = {};
+        storage.report.total = 0;
+        storage.report.epics = [];
+        storage.report.issues = {};
+
+        for (let issue of jiras.issues) {
+            let key = issue.fields.assignee.key;
+            let data = {};
+
+            storage.report.total++;
+
+            if (!storage.report.assignee[key]) {
+                storage.report.assignee[key] = {};
+                storage.report.assignee[key].aggregatetimeoriginalestimate = 0;
+                storage.report.assignee[key].aggregatetimespent = 0;
+                storage.report.assignee[key].index = 0;
+                storage.report.assignee[key].total = 0;
+                storage.report.assignee[key].displayName = '';
+                storage.report.assignee[key].data = [];
+            }
+
+            storage.report.assignee[key].displayName = issue.fields.assignee.displayName;
+            storage.report.assignee[key].aggregatetimeoriginalestimate += issue.fields.aggregatetimeoriginalestimate;
+            storage.report.assignee[key].aggregatetimespent += issue.fields.aggregatetimespent;
+            storage.report.assignee[key].isSubtask = issue.fields.issuetype.subtask;
+            storage.report.assignee[key].total++;
+
+            data.aggregatetimeoriginalestimate = issue.fields.aggregatetimeoriginalestimate;
+            data.aggregatetimespent = issue.fields.aggregatetimespent;
+
+            if (storage.report.assignee[key].isSubtask) {
+                issue = await getParent(issue);
+            }
+
+            data.epicName = await getEpicName(issue, epic, storage.url);
+            storage.report.epics.push(data.epicName);
+
+            storage.report.assignee[key].data.push(data);
+        }
+
+        for (let [key, data] of Object.entries(storage.report.assignee)) {
+            let report = storage.report.assignee[key];
+
+            report.index = report.aggregatetimeoriginalestimate / report.aggregatetimespent;
+        }
+
+        storage.report.epics = [...new Set(storage.report.epics)];
+
+        console.log(storage);
+
     };
 
     let createReport = (data) => {
