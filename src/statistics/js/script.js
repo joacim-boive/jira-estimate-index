67a095e7e8c6c3a2e9ee23134542df94b5617851
joacim.boive@gmail.com <gsxr750>
2017-04-12 14:42:34 +0200

    Fixed epic stats Working on waiting notifications

--- src/statistics/js/script.js
+++ src/statistics/js/script.js
@@ -6,6 +6,49 @@
 
     let headers;
     let report;
+    let notify = (config) => {
+        $.notify({
+            // options
+            icon: config.icon ? 'glyphicon ' + config.icon : '',
+            title: config.title || '',
+            message: config.message || '',
+            url: config.url || '',
+            target: '_blank'
+        }, {
+            // settings
+            element: 'body',
+            type: config.type,
+            allow_dismiss: true,
+            newest_on_top: true,
+            showProgressbar: false,
+            placement: {
+                from: "top",
+                align: "right"
+            },
+            offset: 20,
+            spacing: 10,
+            z_index: 1031,
+            delay: 7000,
+            timer: 1000,
+            url_target: '_blank',
+            mouse_over: true,
+            animate: {
+                enter: 'animated fadeInDown',
+                exit: 'animated fadeOutUp'
+            },
+            icon_type: 'class',
+            template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0}" role="alert">' +
+            '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+            '<span data-notify="icon"></span> ' +
+            '<span data-notify="title">{1}</span> ' +
+            '<div>&nbsp;</div><span data-notify="message">{2}</span>' +
+            '<div class="progress" data-notify="progressbar">' +
+            '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+            '</div>' +
+            '<a href="{3}" target="{4}" data-notify="url"></a>' +
+            '</div>'
+        });
+    };
 
     let getEpicInfo = (url) => {
         return fetch(`${url}/rest/api/2/field`, {
@@ -74,8 +117,51 @@
                 return response.json()
             })
             .then(thisEpic => {
-                return thisEpic.fields[epic.name];
+                return thisEpic;
             })
+    };
+
+    let quoteCount = 0;
+
+    let doTheChuck = () => {
+        return fetch('https://api.chucknorris.io/jokes/random', {
+            method: 'GET',
+            redirect: 'follow'
+        })
+            .then(response => {
+                return response.json()
+            });
+    };
+
+    let getQuote = async () => {
+        let quote = await doTheChuck();
+        let titles = [
+            '<div>This is taking some time...</div><div>Enjoy this Chuck Norris quote while you wait:</div>',
+            '<div>It\'s not called the World Wide Wait for nothing!</div><div>Here\'s another quote:</div>',
+            '<div>Knock-Knock! Who\'s there?</div><div>It\'s another Chuck Norris quote:</div>',
+            '<div>Should we sing a song while we wait?</div><div>Nah, let\'s do another Chuck Norris quote:</div>',
+            '<div>No, y\'ruoe not mroe itneillengt bceuase you can raed tihs</div><div>Here\'s another quote to make you feel better:</div>',
+            '<div>I actually get payed to do stuff like this!</div><div>Let\'s celebrate that with another Chuck Norris quote:</div>',
+            '<div>Guess what?!</div><div>It\s Chuck Norris quote time (again):</div>',
+            '<div>What are you still doing here..?</div><div>Here\'s another Chuck Norris quote, now leave me alone:</div>'
+        ];
+
+        if (quoteCount === 0) {
+            notify({
+                type: 'info',
+                title: '<div>This is taking some time...</div><div>Enjoy this Chuck Norris quote while you wait:</div>',
+                message: quote.value
+            });
+        } else {
+            notify({
+                type: 'info',
+                title: '<div>It\'s not called the World Wide Wait for nothing!</div><div>Here\'s another quote:</div>',
+                message: `<div>${quote.value}</div>`,
+                url: quote.url
+            });
+        }
+
+        return setTimeout(getQuote, 5000);
     };
 
     let getIndex = async (storage) => {
@@ -84,17 +170,19 @@
             'Content-Type': 'application/json'
         });
 
+        let whileYouWait = setTimeout(getQuote, 5000);
+
         const epic = await getEpicInfo(storage.url);
         const jiras = await getJIRAs(storage.url, storage.jql);
 
-        let map = new Map();
+        let mapUser = new Map();
+        let mapEpic = new Map();
 
         storage.fromDate = new Date(storage.fromDate);
         storage.toDate = new Date(storage.toDate);
         storage.report = {};
         storage.report.assignee = [];
         storage.report.total = 0;
-        storage.report.epics = [];
         storage.report.issues = {};
 
         for (let issue of jiras.issues) {
@@ -102,6 +190,8 @@
             let key = '';
             let data = {};
             let user = {};
+            let thisEpic = {};
+            let thatEpic = {};
 
             if (assignee) {
                 key = assignee.key;
@@ -112,8 +202,8 @@
 
             storage.report.total++;
 
-            if (!map.has(key)) {
-                map.set(key, {
+            if (!mapUser.has(key)) {
+                mapUser.set(key, {
                     aggregatetimeoriginalestimate: 0,
                     aggregatetimespent: 0,
                     index: 0,
@@ -123,7 +213,7 @@
                 });
             }
 
-            user = map.get(key);
+            user = mapUser.get(key);
 
             user.aggregatetimeoriginalestimate += issue.fields.aggregatetimeoriginalestimate;
             user.aggregatetimespent += issue.fields.aggregatetimespent;
@@ -136,29 +226,56 @@
                 issue = await getParent(issue);
             }
 
-            data.epicName = await getEpicName(issue, epic, storage.url);
+            thisEpic = await getEpicName(issue, epic, storage.url);
+
+            data.epicName = thisEpic.fields ? thisEpic.fields[epic.name] : thisEpic;
+
             data.isSubtask = issue.fields.issuetype.subtask;
 
-            storage.report.epics.push(data.epicName);
-
             user.data.push(data);
+
+            if (!mapEpic.has(data.epicName)) {
+                mapEpic.set(data.epicName, {
+                    aggregatetimeoriginalestimate: 0,
+                    aggregatetimespent: 0,
+                    index: 0,
+                    total: 0,
+                    displayName: data.epicName,
+                    data: []
+                });
+            }
+
+            thatEpic = mapEpic.get(data.epicName);
+
+            thatEpic.aggregatetimeoriginalestimate += issue.fields.aggregatetimeoriginalestimate;
+            thatEpic.aggregatetimespent += issue.fields.aggregatetimespent;
+
+            thatEpic.total++;
+            thatEpic.data.push(user);
+
 
             report = storage.report;
         }
 
-        storage.report.assignee = map;
+        clearInterval(whileYouWait);
 
-        for (let key of map.keys()) {
-            let user = map.get(key);
+        storage.report.assignee = mapUser;
+
+        for (let key of mapUser.keys()) {
+            let user = mapUser.get(key);
 
             user.index = user.aggregatetimeoriginalestimate / user.aggregatetimespent
         }
 
-        storage.report.epics = [...new Set(storage.report.epics)];
+        for (let key of mapEpic.keys()) {
+            let epic = mapEpic.get(key);
 
-        storage.report.activeEpics = storage.report.epics;
+            epic.index = epic.aggregatetimeoriginalestimate / epic.aggregatetimespent
+        }
 
-        if(createEpics(storage)){
+        storage.report.epics = mapEpic;
+
+        if (createEpics(storage)) {
             createReport(storage.report);
         }
     };
@@ -169,12 +286,14 @@
 
     let createEpics = (data) => {
         const epics = data.report.epics;
+        let epicId = '';
         let epicButtons = '';
         let isChecked = false;
         let hasDisabledEpics = false;
 
-        for (let epic of epics) {
-            isChecked = data[getWebSafeName(epic)];
+        for (let [key, info] of epics) {
+            epicId = getWebSafeName(info.displayName);
+            isChecked = data[epicId];
 
             if (typeof(isChecked) !== 'boolean') {
                 isChecked = true;
@@ -185,7 +304,7 @@
             }
 
             epicButtons += `<label class="btn btn-primary${isChecked ? ' active' : ''}">
-                        <input type="checkbox" autocomplete="off" ${isChecked ? 'checked': ''} data-name="${epic}" id="${getWebSafeName(epic)}"> ${epic}
+                        <input type="checkbox" autocomplete="off" ${isChecked ? 'checked' : ''} data-name="${info.displayName}" id="${epicId}"> ${info.displayName}
                       </label>`;
         }
 
@@ -197,14 +316,12 @@
 
             }, 300);
             return false;
-        }else{
+        } else {
             return true;
         }
     };
 
-    let createReport = (data) => {
-        let holder = document.getElementById('user-stats');
-        let loading = document.getElementById('loading');
+    let reportUser = (data) => {
         let html = '<table class="table table-striped table-bordered table-hover"><tr><th>User</th><th>Index</th><th>Epics</th></tr>';
         let memberOfEpics;
         let users = Array.from(data.assignee);
@@ -233,11 +350,40 @@
         }
 
 
-        html += '</tr></table>';
-        holder.innerHTML = html;
+        return html += '</tr></table>';
+    };
+
+    let reportEpic = (data) => {
+        let html = '<table class="table table-striped table-bordered table-hover"><tr><th>Epic</th><th>Index</th><th>Count</th></tr>';
+        let epics = Array.from(data.epics);
+
+        epics.sort((a, b) => {
+            return parseFloat(b[1].index) - parseFloat(a[1].index); //Sort in descending order.
+        });
+
+        for (const [index, thisData] of epics.entries()) {
+            let key = thisData[0];
+            let info = thisData[1];
+
+            html += `<tr><td>${info.displayName}</td><td>${info.index}</td><td>${info.total}</td></tr>`
+        }
+
+
+        return html += '</tr></table>';
+    };
+
+
+    let createReport = (data) => {
+        let loading = document.getElementById('loading');
+        let holderUser = document.getElementById('user-stats');
+        let holderEpic = document.getElementById('epic-stats');
+
+        holderUser.innerHTML = reportUser(data);
+        holderEpic.innerHTML = reportEpic(data);
 
         loading.classList.add('hide');
-        holder.classList.remove('hide');
+        holderUser.classList.remove('hide');
+        holderEpic.classList.remove('hide');
     };
 
     let getStorage = () => {
@@ -246,7 +392,6 @@
         loading.classList.remove('hide');
 
         chrome.storage.local.get(function (storage) {
-            // getIndex(storage).then(data => createReport(data));
             getIndex(storage)
         })
     };
